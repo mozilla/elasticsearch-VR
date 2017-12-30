40c103c78cca67fbb396ae7a8b29503b456cf112
Jeff Bryner <jeff@jeffbryner.com>
2017-12-29 20:04:17 -0800

    better logging on connect/error to es cluster

--- vr/elastic-vr.js
+++ vr/elastic-vr.js
@@ -1,24 +1,26 @@
 
 d3.json("/info", function(error, jsondata) {
-    console.log(error);
-    console.log(jsondata);
-    window.jsondata=jsondata;
-    var escluster = d3.select('#escluster');
-    var esnodes = Math.max(3,jsondata.clusterstatus.number_of_data_nodes)
-    for (var i = 0; i < esnodes ; i++) {
-        AFRAME.log("building an es node",'eslog');
-        escluster.append('a-box')
-          .classed('esserver',true)
-          .attr('height', '1')
-          .attr('material', 'src: #estexture;')
-          .append('a-animation')
-            .attr('attribute',"scale")
-            .attr('direction',"alternate-reverse")
-            .attr('dur',Math.max(4000,Math.floor(Math.random(1)*10000)))
-            .attr('from',"1 1 1")
-            .attr('to',"1.2 1.2 1.2")
-            .attr('repeat',"indefinite");
+    if (error){
+        AFRAME.log(error,'eslog');
+    }
+    else{
+        var escluster = d3.select('#escluster');
+        var esnodes = Math.max(3,jsondata.clusterstatus.number_of_data_nodes)
+        for (var i = 0; i < esnodes ; i++) {
+            AFRAME.log("building an es node",'eslog');
+            escluster.append('a-box')
+            .classed('esserver',true)
+            .attr('height', '1')
+            .attr('material', 'src: #estexture;')
+            .append('a-animation')
+                .attr('attribute',"scale")
+                .attr('direction',"alternate-reverse")
+                .attr('dur',Math.max(4000,Math.floor(Math.random(1)*10000)))
+                .attr('from',"1 1 1")
+                .attr('to',"1.2 1.2 1.2")
+                .attr('repeat',"indefinite");
 
+        }
     }
 
 });
