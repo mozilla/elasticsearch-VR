86cd3ce2955ba51be24e04ac6eb228cf98f4eaa8
Jeff Bryner <jeff@jeffbryner.com>
2017-12-29 18:51:00 -0800

    add layout to es cluster nodes for proper handling of > 1 node, set min nodes

--- vr/elastic-vr.js
+++ vr/elastic-vr.js
@@ -3,20 +3,18 @@ d3.json("/info", function(error, jsondata) {
     console.log(error);
     console.log(jsondata);
     window.jsondata=jsondata;
-    var scene = d3.select('a-scene');
-    for (var i = 0; i < jsondata.clusterstatus.number_of_data_nodes; i++) {
+    var escluster = d3.select('#escluster');
+    var esnodes = Math.max(3,jsondata.clusterstatus.number_of_data_nodes)
+    for (var i = 0; i < esnodes ; i++) {
         console.log("building an es node")
-        scene.append('a-box')
+        escluster.append('a-box')
           .classed('esserver',true)
-          .attr('visible', 'true')
-          .attr('position', "1 5 -10")
           .attr('height', '1')
           .attr('material', 'src: #estexture;')
-          .attr('dynamic-body','mass:500;linearDamping:.9;angularDamping:.9;')
           .append('a-animation')
             .attr('attribute',"scale")
             .attr('direction',"alternate-reverse")
-            .attr('dur',"5000")
+            .attr('dur',Math.min(3000,Math.floor(Math.random(1)*10000)))
             .attr('from',"1 1 1")
             .attr('to',"1.2 1.2 1.2")
             .attr('repeat',"indefinite");
