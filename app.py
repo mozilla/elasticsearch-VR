bf35dfb0b09719e3b3f54ed2295e4d651be947ee
Jeff Bryner <jeff@jeffbryner.com>
2017-12-29 20:11:48 -0800

    add indices endpoint

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from elasticsearch.exceptions import NotFoundError
 from elasticsearch.helpers import bulk, BulkIndexError
-
 from elasticsearch_client import ElasticsearchClient, ElasticsearchBadServer, ElasticsearchInvalidIndex, ElasticsearchException
 from utilities.toUTC import toUTC
 from query_models import SearchQuery, TermMatch, AggregatedResults, SimpleResults
-
 
 from flask import (
     Flask,
@@ -30,7 +28,6 @@ from flask import (
 
 # setup the app
 app = Flask(__name__)
-#app.config.from_pyfile('env.example')
 
 #logging
 logger = logging.getLogger(__name__)
@@ -59,8 +56,6 @@ else:
 #connect to ES
 esConnecton = ElasticsearchClient((list('{0}'.format(s) for s in app.config['ES_SERVERS'].split(','))), 10)
 logger.info(app.config['ES_SERVERS'])
-#logger.info(esConnecton.get_cluster_health())
-
 
 @app.route('/info')
 def info():
@@ -69,21 +64,31 @@ def info():
         clusterstatus=esConnecton.get_cluster_health()
     )
 
+@app.route('/indices')
+def indices():
+    """return the indexes in the cluster"""
+    return jsonify(
+        indices=esConnecton.get_indices()
+    )
+
 @app.route('/')
 def main_page():
+    """main page"""
     return render_template("main_page.html")
 
 
 @app.route("/vr/<path:filename>")
 def vr_file(filename):
+    """serve any file in /vr"""
     return send_from_directory('vr/',
                                filename)
 
 @app.route("/assets/<path:filename>")
 def assets_file(filename):
+    """serve any file in /assets"""
     return send_from_directory('assets/',
                                filename)
 
-# We only need this for local development.
+# for local development.
 if __name__ == '__main__':
     app.run()
