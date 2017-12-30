1b1c66967d94b28e92bd7e693cc3b31df451543e
Jeff Bryner <jeff@jeffbryner.com>
2017-12-29 19:10:28 -0800

    add logging viewpoint

--- vr/elastic-vr.js
+++ vr/elastic-vr.js
@@ -6,7 +6,7 @@ d3.json("/info", function(error, jsondata) {
     var escluster = d3.select('#escluster');
     var esnodes = Math.max(3,jsondata.clusterstatus.number_of_data_nodes)
     for (var i = 0; i < esnodes ; i++) {
-        console.log("building an es node")
+        AFRAME.log("building an es node",'eslog');
         escluster.append('a-box')
           .classed('esserver',true)
           .attr('height', '1')
@@ -14,7 +14,7 @@ d3.json("/info", function(error, jsondata) {
           .append('a-animation')
             .attr('attribute',"scale")
             .attr('direction',"alternate-reverse")
-            .attr('dur',Math.min(3000,Math.floor(Math.random(1)*10000)))
+            .attr('dur',Math.min(5000,Math.floor(Math.random(1)*10000)))
             .attr('from',"1 1 1")
             .attr('to',"1.2 1.2 1.2")
             .attr('repeat',"indefinite");
